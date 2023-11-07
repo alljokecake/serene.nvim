c62d1b4f767b2f12d19be4ea2b82bc4a34f250db
schlanqe <ilhanburcakbas@gmail.com>
2023-11-07 10:11:25 +0300

    inner peace

---
+++ lua/serene/init.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+M.toggle_options = function()
+    local options = {
+        { name = "signcolumn",      type = "string" },
+        { name = "number",          type = "boolean" },
+        { name = "relativenumber",  type = "boolean" },
+        { name = "cursorline",      type = "boolean" },
+    }
+
+    for _, option in ipairs(options) do
+        local current_option_value = vim.o[option.name]
+
+        if option.type == "string" then
+            vim.o[option.name] = (current_option_value == "no") and "yes" or "no"
+        elseif option.type == "boolean" then
+            vim.o[option.name] = not current_option_value
+        end
+    end
+end
+
+return M
